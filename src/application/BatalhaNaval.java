98befe10aebbafa3173ea947142ef97138ee3c3f
Brenda Stephanie <brendasod98@gmail.com>
2022-12-07 13:52:57 -0300

    Criando a matriz 10x10.

--- src/application/BatalhaNaval.java
+++ src/application/BatalhaNaval.java
@@ -1,10 +1,16 @@
 package application;
 
+import entities.Tabuleiro;
+
 public class BatalhaNaval {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Tabuleiro tabuleiro = new Tabuleiro();
+		
+		tabuleiro.criaTabuleiro();
+		tabuleiro.mostraTabuleiro();
+			
 	}
 
 }
--- src/entities/Tabuleiro.java
+++ src/entities/Tabuleiro.java
@@ -1,5 +1,25 @@
 package entities;
 
 public class Tabuleiro {
+	
+	private int[][] tabuleiro = new int[10][10];
+	
+	public void criaTabuleiro() {
+		for(int linha = 0; linha < 10; linha++) {
+			for(int coluna = 0; coluna < 10; coluna++) {
+				tabuleiro[linha][coluna] = 2;
+			}
+		}
+	}
+	
+	public void mostraTabuleiro() {
+		for(int linha = 0; linha < 10; linha++) {
+			for(int coluna = 0; coluna < 10; coluna++) {
+				System.out.print(tabuleiro[linha][coluna] + " ");
+			}			
+			System.out.println();
+		}
+		
+	}
 
 }
