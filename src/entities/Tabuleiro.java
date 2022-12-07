a64b4e557c21cb1e1d88745bfee0c044bb005d90
Brenda Stephanie <brendasod98@gmail.com>
2022-12-07 14:50:16 -0300

    Aplicando condições no tabuleiro para mostrar um tabuleiro diferente caso o jogador acerte ou erre as embarcações.

--- src/entities/Tabuleiro.java
+++ src/entities/Tabuleiro.java
@@ -7,15 +7,26 @@ public class Tabuleiro {
 	public void criaTabuleiro() {
 		for(int linha = 0; linha < 10; linha++) {
 			for(int coluna = 0; coluna < 10; coluna++) {
-				tabuleiro[linha][coluna] = 2;
+				tabuleiro[linha][coluna] = 0;
 			}
 		}
 	}
 	
+	/*
+	 *  Mostrando o tabuleiro com condições onde 
+	 * ~ é agua e sem navio, * é um tiro onde não tem navio(errou)
+	 *  e X é tiro onde tem navio(acertou)
+	 */
 	public void mostraTabuleiro() {
 		for(int linha = 0; linha < 10; linha++) {
 			for(int coluna = 0; coluna < 10; coluna++) {
-				System.out.print(tabuleiro[linha][coluna] + " ");
+				if(tabuleiro[linha][coluna] == 0) {
+					System.out.print(" ~ ");
+				} else if(tabuleiro[linha][coluna] == 1) {
+					System.out.print(" * ");
+				} else if(tabuleiro[linha][coluna] == 2) {
+					System.out.print(" X ");
+				}
 			}			
 			System.out.println();
 		}
